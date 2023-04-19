e3f6d7da3ff91b3e6ee8f5cf82af6e545358148e
Scoheart <Scoheart1487790559@gmail.com>
2023-04-20 00:10:24 +0800

    feat: add new templates

--- packages/tools/index.js
+++ packages/tools/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 axios({
     method: "GET",
-    url: "https://github.com/scoheart/create-github-api2/tree/main/packages/templates"
-}).then(res => console.log(res.date))
+    url: "https://api.github.com/repos/scoheart/create-github-api2/contents/packages/templates/react"
+}).then(res => console.log(res.data))
